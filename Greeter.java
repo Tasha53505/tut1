babfe972b15c839145d618a5eb4f497a8074a250
Tasha Carr <ncarr63@gmail.com>
2024-07-22 12:06:28 +1200

    Added Fdeeqature 1

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", (bug is fixed)" + name;
+    return messages.getString("greeting") + ", (bug is fixed)" + name.toUpperCase();
   }
 
   public static void main(String[] args) {
